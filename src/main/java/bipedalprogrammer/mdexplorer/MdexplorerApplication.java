273a4599bcc5e7e5e0c6698ee35df92c9c1ecd1f
Dennis Caldwell <dcaldwell@lumafintech.com>
2020-11-21 13:36:00 -0500

    Spring Boot with Flexmark dependencies.

---
+++ src/main/java/bipedalprogrammer/mdexplorer/MdexplorerApplication.java
@@ -0,0 +1,13 @@
+package bipedalprogrammer.mdexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdexplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdexplorerApplication.class, args);
+    }
+
+}
